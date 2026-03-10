233dd1d5980dd70a437fd7c56c1c6ff0b6fd4c31
leeejunu <leeejunu@gmail.com>
2026-03-10 22:03:22 +0900

    feat: user-service를 gateway에 추가

---
+++ gateway-mvc/src/main/java/com/example/gatewaymvc/GatewayMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewaymvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMvcApplication.class, args);
+	}
+
+}
---
+++ gateway-mvc/src/test/java/com/example/gatewaymvc/GatewayMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaymvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
