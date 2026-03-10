7c70af0057c76aee3ab18becc3c78e039cff2462
leeejunu <leeejunu@gmail.com>
2026-03-10 22:17:19 +0900

    feat: gateway-webflux 설정 추가

---
+++ gateway-webflux/src/main/java/com/example/gateway_webflux/GatewayWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.gateway_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayWebfluxApplication.class, args);
+	}
+
+}
---
+++ gateway-webflux/src/test/java/com/example/gateway_webflux/GatewayWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gateway_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
